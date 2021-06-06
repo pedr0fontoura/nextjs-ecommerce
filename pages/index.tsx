7ce7b43d0c1dab96b17ed89f178cb80a3eea409e
pedr0fontoura <pedrofontoura@id.uff.br>
2021-06-06 14:46:28 -0300

    feat: add shopping cart

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
@@ -6,7 +7,72 @@ import { initiateCheckout } from "../lib/payments";
 
 import products from "../products.json";
 
+interface IProduct {
+  id: string;
+  quantity: number;
+}
+
+interface ICart {
+  products: IProduct[];
+}
+
+const defaultCart = {
+  products: [],
+};
+
 export default function Home() {
+  const [cart, setCart] = useState<ICart>(defaultCart);
+
+  const cartItems = cart.products.map((cartProduct) => {
+    const product = products.find(({ id }) => id === cartProduct.id);
+
+    return {
+      ...cartProduct,
+      pricePerItem: product?.price,
+    };
+  });
+
+  const subtotal = cartItems.reduce(
+    (accumulator, { pricePerItem = 0, quantity }) => {
+      return accumulator + pricePerItem * quantity;
+    },
+    0
+  );
+
+  const totalItems = cartItems.reduce((accumulator, { quantity }) => {
+    return accumulator + quantity;
+  }, 0);
+
+  function addToCart({ id }: { id: string }): void {
+    setCart((previous) => {
+      let cartState = { ...previous };
+
+      const productIndex = cartState.products.findIndex(
+        (product) => product.id === id
+      );
+
+      if (productIndex !== -1) {
+        cartState.products[productIndex].quantity += 1;
+      } else {
+        cartState.products.push({
+          id,
+          quantity: 1,
+        });
+      }
+
+      return cartState;
+    });
+  }
+
+  function checkout(): void {
+    initiateCheckout({
+      lineItems: cartItems.map((item) => ({
+        price: item.id,
+        quantity: item.quantity,
+      })),
+    });
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -22,6 +88,16 @@ export default function Home() {
           The best space jellyfish swag on the web!
         </p>
 
+        <p className={styles.description}>
+          Items: {totalItems}
+          <br />
+          Total Cost: R${subtotal}
+          <br />
+          <button className={styles.button} onClick={checkout}>
+            Check Out
+          </button>
+        </p>
+
         <ul className={styles.grid}>
           {products.map((product) => (
             <li key={product.id} className={styles.card}>
@@ -34,18 +110,9 @@ export default function Home() {
               <p>
                 <button
                   className={styles.button}
-                  onClick={() =>
-                    initiateCheckout({
-                      lineItems: [
-                        {
-                          price: product.id,
-                          quantity: 1,
-                        },
-                      ],
-                    })
-                  }
+                  onClick={() => addToCart({ id: product.id })}
                 >
-                  Buy Now
+                  Add to Cart
                 </button>
               </p>
             </li>
